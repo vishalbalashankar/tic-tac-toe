33cc7f89c73e78ee3c157129c7938fa229389563
vishalbalashankar <vishal3@ualberta.ca>
2020-12-17 05:52:19 +0000

    Update status message

--- src/index.js
+++ src/index.js
@@ -40,7 +40,8 @@ class Board extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + 
+      (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
