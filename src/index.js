11f4f4091d46e37a06c7f3f500d3ac3fb7ff34ed
vishalbalashankar <vishal3@ualberta.ca>
2020-12-17 06:02:22 +0000

    Update status to reflect winner

--- src/index.js
+++ src/index.js
@@ -40,9 +40,14 @@ class Board extends React.Component {
     }
   
     render() {
-      const status = 'Next player: ' + 
-      (this.state.xIsNext ? 'X' : 'O');
-  
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+          status = 'Winner: ' + winner;
+      } else {
+          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+        
       return (
         <div>
           <div className="status">{status}</div>
